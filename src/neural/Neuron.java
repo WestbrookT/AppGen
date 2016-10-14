ed822a145fa3bc11b5065d95e390ae8538c760f7
unknown <wam806@T0000B054.cs.utsarr.net>
2016-10-14 11:02:44 -0500

    Started project structure

---
+++ src/neural/Neuron.java
@@ -0,0 +1,20 @@
+package neural;
+
+import java.util.Random;
+
+public class Neuron {
+	
+	int [] weights = null;
+	
+	public Neuron(int weightCount) {
+		weights = new int[weightCount];
+		
+		
+		
+		for (int i = 0; i < weightCount; i++)
+		{
+			
+		}
+	}
+
+}
