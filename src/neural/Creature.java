013a81d6d5433c64664de2b0568771515c08a1f8
Joshua <josue_mx_2012@live.com>
2016-10-14 12:32:00 -0500

    before class on 10/14

---
+++ src/neural/Creature.java
@@ -0,0 +1,31 @@
+package neural;
+
+public class Creature {
+	
+	private int size;
+	private NeuralNet brain;
+	private int[] color;
+	private int xPos;
+	private int yPos;
+	private WorldGrid world;
+	
+	private void eat() {
+		
+	}
+	
+	private int move() {
+		
+	}
+	
+	public void act() {
+		
+	}
+	
+	public double[][] run() {
+		
+	}
+	
+	private Creature reproduce() {
+		
+	}
+}
---
+++ src/tile/Tile.java
@@ -0,0 +1,59 @@
+package tile;
+
+public class Tile {
+	
+	private int r;
+	private int g;
+	private int b;
+	private int maxR;
+	private int maxG;
+	private int maxB;
+	
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	public int getG() {
+		return g;
+	}
+
+	public void setG(int g) {
+		this.g = g;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int getMaxR() {
+		return maxR;
+	}
+
+	public void setMaxR(int maxR) {
+		this.maxR = maxR;
+	}
+
+	public int getMaxG() {
+		return maxG;
+	}
+
+	public void setMaxG(int maxG) {
+		this.maxG = maxG;
+	}
+
+	public int getMaxB() {
+		return maxB;
+	}
+
+	public void setMaxB(int maxB) {
+		this.maxB = maxB;
+	}
+}
