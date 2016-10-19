8ae22b028dd99203603bad195ab81124b06fdb8c
Joshua <josue_mx_2012@live.com>
2016-10-19 12:52:09 -0500

    change

---
+++ src/command/CommandController.java
@@ -0,0 +1,13 @@
+package command;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+public class CommandController extends JFrame{
+	
+	private JButton LoadButton;
+	private JButton SpeedButton;
+	private JButton SaveButton;
+	private JButton PauseButton;
+
+}
