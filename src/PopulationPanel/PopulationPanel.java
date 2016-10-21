dfa42c81132677f73a8f1dd6d3c2d6aad645c549
Tyler Mead <tmead737@gmail.com>
2016-10-21 12:27:01 -0500

    Added PopulationPanel

---
+++ src/PopulationPanel/PopulationPanel.java
@@ -0,0 +1,171 @@
+package PopulationPanel;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PopulationPanel extends JPanel {
+
+    private int width = 800;
+    private int heigth = 400;
+    private int padding = 25;
+    private int labelPadding = 25;
+    private Color lineColor = new Color(44, 102, 230, 180);
+    private Color pointColor = new Color(100, 100, 100, 180);
+    private Color gridColor = new Color(200, 200, 200, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private int pointWidth = 4;
+    private int numberYDivisions = 10;
+    private ArrayList<Integer> pop = new ArrayList <Integer> ();
+    private int [] population = {1,2,6,124,32,5645,856,190};
+    
+    
+
+    public PopulationPanel(ArrayList<Integer> temp) {
+        this.pop = temp;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (pop.size() - 1);
+        double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMaxPop() - getMinPop());
+
+        ArrayList<Point> graphPoints = new ArrayList<>();
+        for (int i = 0; i < pop.size(); i++) {
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMaxPop() - pop.get(i)) * yScale + padding);
+            graphPoints.add(new Point(x1, y1));
+        }
+
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
+
+        // create hatch marks and grid lines for y axis.
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (pop.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMinPop() + (getMaxPop() - getMinPop()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
+
+        // and for x axis
+        for (int i = 0; i < pop.size(); i++) {
+            if (pop.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding) / (pop.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if (i % ((pop.size() / 20) + 1) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+                }
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+
+        // create x and y axes 
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding);
+
+        Stroke oldStroke = g2.getStroke();
+        g2.setColor(lineColor);
+        g2.setStroke(GRAPH_STROKE);
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
+        }
+
+        g2.setStroke(oldStroke);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size(); i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            int ovalW = pointWidth;
+            int ovalH = pointWidth;
+            g2.fillOval(x, y, ovalW, ovalH);
+        }
+    }
+
+//    @Override
+//    public Dimension getPreferredSize() {
+//        return new Dimension(width, heigth);
+//    }
+    private double getMinPop() {
+        double minPop = Integer.MAX_VALUE;
+        for (int population : pop) {
+            minPop = Math.min(minPop, population);
+        }
+        return minPop;
+    }
+
+    private double getMaxPop() {
+        double maxPop = Integer.MIN_VALUE;
+        for (int population : pop) {
+            maxPop = Math.max(maxPop, population);
+        }
+        return maxPop;
+    }
+
+    public void setPop(ArrayList <Integer> pop, int [] population) {
+        this.pop = pop;
+        for(int temp : population){
+        	pop.add(temp);
+        }
+        invalidate();
+        this.repaint();
+    }
+
+    public ArrayList <Integer> getPop() {
+        return pop;
+    }
+
+    private static void createAndShowGui() {
+        ArrayList <Integer> pop = new ArrayList <Integer> ();
+        pop.add(1);
+        pop.add(25);
+        pop.add(17);
+        pop.add(29);
+        
+        
+        PopulationPanel mainPanel = new PopulationPanel(pop);
+        mainPanel.setPreferredSize(new Dimension(800, 600));
+        JFrame frame = new JFrame("Population");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+      SwingUtilities.invokeLater(new Runnable() {
+         public void run() {
+            createAndShowGui();
+         }
+      });
+   }
+}
