53821ab388b2be22b8676dfea46292ac9f0797bc
Joshua <josue_mx_2012@live.com>
2016-10-31 12:31:17 -0500

    push

--- src/command/SaveState.java
+++ src/command/SaveState.java
@@ -4,20 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.swing.AbstractButton;
 
+import neural.Creature;
+
 public class SaveState implements ActionListener, java.io.Serializable {
 	
 	AbstractButton button;
-	File file = new File("saved data.prop");
-<<<<<<< HEAD
-=======
-	//JSONObject save = new JSONObject();
->>>>>>> 3344c6376aedf2106014c5330127dad2b8ae002a
 	
 	public SaveState(AbstractButton button) {
 		this.button = button;
@@ -25,34 +25,15 @@ public class SaveState implements ActionListener, java.io.Serializable {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		//save.setProperty(ArrayList<Creature>, creature);
 		
-//		BufferedWriter pen;
-//		try {
-//			pen = new BufferedWriter(new FileWriter(file));
-//		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-<<<<<<< HEAD
-//			e1.printStackTrace();
-//		}
-//		try {
-//			save.store(pen, "properties of creatures");
-//		} catch (IOException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-=======
-			e1.printStackTrace();
-		}
+		ArrayList<Creature> creatures = new ArrayList<Creature>();
+		
+		
 		try {
-			//save.store(pen, "properties of creatures");
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			FileOutputStream fout = new FileOutputStream("SavedData.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			
 		}
->>>>>>> 3344c6376aedf2106014c5330127dad2b8ae002a
-		
 	}
 
 }
