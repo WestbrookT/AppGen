6c765c64bf589e8082d18b2bd6383b766222a0b5
Luke Taylor <LTaylor255@gmail.com>
2016-11-09 12:45:21 -0600

    Connected World Panel to main, draws creatures as ovals

--- src/worldPanel/WorldPanel.java
+++ src/worldPanel/WorldPanel.java
@@ -4,26 +4,20 @@ import java.awt.*;
 import java.util.*;
 import javax.swing.*;
 
+import neural.Creature;
 import tile.Tile;
 import tile.WorldGrid;
 
 public class WorldPanel extends JPanel {
 
-    private int width = 800;
-    private int height = 400;
-    private int padding = 25;
-    private int labelPadding = 25;
-    private Color lineColor = new Color(44, 102, 230, 180);
-    private Color pointColor = new Color(100, 100, 100, 180);
-    private Color gridColor = new Color(200, 200, 200, 200);
-    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
-    private int pointWidth = 4;
-    private int numberYDivisions = 10;
     private WorldGrid grid;
+    private ArrayList<Creature> creatureList;
+    private int tileSize;
     
-
     public WorldPanel(WorldGrid wg) {
-        this.grid = wg;
+        grid = wg;
+        creatureList = grid.getCreatures();
+        tileSize = grid.getTileSize();
     }
 
     @Override
@@ -35,34 +29,16 @@ public class WorldPanel extends JPanel {
         for(int x=0;x<tilesGrid.length;x++) {
         	for(int y=0;y<tilesGrid[x].length;y++) {
         		g2.setColor(new Color(tilesGrid[x][y].getR(),tilesGrid[x][y].getG(),tilesGrid[x][y].getB()));
-                g2.drawRect(x*tilesGrid.length/width, y*tilesGrid[x].length/height, 10, 10);
-                /* TODO: Change size of rects, make colored worldgrid */
+                g2.fillRect(x*tileSize, y*tileSize, tileSize, tileSize);
         	}
+        }
+        for(Creature c : creatureList) {
+        	g2.setColor(Color.BLACK);
+        	g2.fillOval(c.getX(), c.getY(),tileSize,tileSize);
         }
     }
 
     public WorldGrid getGrid() {
         return grid;
     }
-
-    private static void createAndShowGui() {
-        WorldGrid wg = new WorldGrid(10,10, 10, 10);
-        //wg.consume(5,5,128,128,128);
-        WorldPanel mainPanel = new WorldPanel(wg);
-        mainPanel.setPreferredSize(new Dimension(800, 600));
-        JFrame frame = new JFrame("World Panel");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(mainPanel);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-      SwingUtilities.invokeLater(new Runnable() {
-         public void run() {
-            createAndShowGui();
-         }
-      });
-   }
 }
