ba481f45397f9b50b9bf93bc3bbe41d9f981ed93
Tyler Mead <tmead737@gmail.com>
2016-12-02 00:35:44 -0600

    restructured main and created View class to better match MVC format

---
+++ src/View/View.java
@@ -0,0 +1,73 @@
+package View;
+
+import java.awt.*;
+import javax.swing.*;
+
+import Model.Model;
+
+import View.AnalysisPanel;
+import View.PopulationPanel;
+import View.WorldPanel;
+
+import command.CreatureSelection;
+import command.LoadState;
+import command.PauseState;
+import command.SaveState;
+import command.SpeedState;
+
+public class View extends JFrame{
+	private JButton LoadButton;
+	private JButton SpeedButton;
+	private JButton SaveButton;
+	private JButton PauseButton;
+	
+	public WorldPanel panel;
+	public PopulationPanel pp;
+	private AnalysisPanel ap;
+
+	private static int speed = 1;
+	
+	public View(Model model){
+		super("Evolutionary Neural Control Simulator");
+		
+		JPanel panelNorth = new JPanel();
+		add(panelNorth, BorderLayout.NORTH);
+		panelNorth.setLayout(new GridLayout(1, 4));
+		panelNorth.setBackground(Color.YELLOW);
+		
+		LoadButton = new JButton("Load");		
+		panelNorth.add(LoadButton);
+		SaveButton = new JButton("Save");
+		panelNorth.add(SaveButton);
+		SpeedButton = new JButton("Speed: x" + speed);
+		panelNorth.add(SpeedButton);
+		PauseButton = new JButton("Pause");
+		panelNorth.add(PauseButton);
+		
+		LoadState loader = new LoadState(LoadButton, model);
+		LoadButton.addActionListener(loader);
+		SpeedState speeder = new SpeedState(SpeedButton, speed, model);
+		SpeedButton.addActionListener(speeder);
+		SaveState saver = new SaveState(SaveButton, model);
+		SaveButton.addActionListener(saver);
+		PauseState pauser = new PauseState(PauseButton, speed, model);
+		PauseButton.addActionListener(pauser);
+	
+		JPanel panelCenter = new JPanel();	
+		panelCenter.setLayout(new BoxLayout(panelCenter,BoxLayout.Y_AXIS));
+		add(panelCenter, BorderLayout.CENTER);
+		panel = new WorldPanel(model);
+		panelCenter.add(panel);
+		
+		
+		JPanel panelBottom = new JPanel();
+		panelBottom.setLayout(new BoxLayout(panelBottom, BoxLayout.LINE_AXIS));
+		pp = new PopulationPanel(model.getWorldGrid());
+		ap = new AnalysisPanel(model.getWorldGrid());
+		panelBottom.add(pp);
+		panelBottom.add(ap);
+		panelCenter.add(panelBottom);
+		
+		panel.addMouseListener(new CreatureSelection(model, ap));
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,205 +1,27 @@
 package main;
 
-
-
-import java.awt.*;
-
-
-import java.awt.event.MouseAdapter;
-
-import java.awt.event.MouseEvent;
-
-
-
-import javax.swing.*;
-
-import javax.swing.text.View;
-
-
-
-import Model.Creature;
-
+import javax.swing.JFrame;
 import Model.Model;
+import View.View;
 
-import Model.WorldGrid;
 
-import View.AnalysisPanel;
-
-import View.PopulationPanel;
-
-import View.WorldPanel;
-
-import command.CreatureSelection;
-
-import command.LoadState;
-
-import command.PauseState;
-
-import command.SaveState;
-
-import command.SpeedState;
+public class Main {
 
 
 
-
-
-public class Main extends JFrame{
-
-
-
-	private JButton LoadButton;
-
-	private JButton SpeedButton;
-	
-	private JButton SaveButton;
-	
-	private JButton PauseButton;
-
-	
-	
-	private Model model;
-	
-	private WorldPanel panel;
-	
-	private PopulationPanel pp;
-	
-	private AnalysisPanel ap;
-	
-	private Creature nearestCreature;
-
-	
-	private static int speed = 1;
-	
-	
-	public Main() {
+	public static void main(String [] args) throws InterruptedException{
+		Model model = new Model(60, 30, 5, 29, 10, 10, 20);
+		View view = new View(model);
 		
-		super("Evolutionary Neural Control Simulator");
+		view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		view.setSize(610, 720);
+		view.setVisible(true);
 		
-		/*JPanel backPanel = new JPanel();
-		
-		backPanel.setLayout(new );
-		
-		*/
-		
-		//buttons for command panel
-		
-		JPanel panelNorth = new JPanel();
-		
-		add(panelNorth, BorderLayout.NORTH);
-		
-		panelNorth.setLayout(new GridLayout(1, 4));
-		
-		panelNorth.setBackground(Color.YELLOW);
-
-		
-		
-		
-		
-		
-		model = new Model(60, 30, 5, 29, 10, 10, 20);
-
-
-		
-		LoadButton = new JButton("Load");
-		
-		panelNorth.add(LoadButton);
-		
-		SaveButton = new JButton("Save");
-		
-		panelNorth.add(SaveButton);
-		
-		SpeedButton = new JButton("Speed: x" + speed);
-		
-		panelNorth.add(SpeedButton);
-		
-		PauseButton = new JButton("Pause");
-		
-		panelNorth.add(PauseButton);
-		
-		LoadState loader = new LoadState(LoadButton, model);
-		
-		LoadButton.addActionListener(loader);
-		
-		SpeedState speeder = new SpeedState(SpeedButton, speed, model);
-		
-		SpeedButton.addActionListener(speeder);
-		
-		SaveState saver = new SaveState(SaveButton, model);
-		
-		SaveButton.addActionListener(saver);
-		
-		PauseState pauser = new PauseState(PauseButton, speed, model);
-		
-		PauseButton.addActionListener(pauser);
-	
-	
-		
-		JPanel panelCenter = new JPanel();
-		
-		panelCenter.setLayout(new BoxLayout(panelCenter,BoxLayout.Y_AXIS));
-		
-		add(panelCenter, BorderLayout.CENTER);
-		
-		panel = new WorldPanel(model);
-		
-		panelCenter.add(panel);
-		
-		
-		pp = new PopulationPanel(model.getWorldGrid());
-		
-		ap = new AnalysisPanel(model.getWorldGrid());
-		
-		JPanel panelBottom = new JPanel();
-		
-		panelBottom.setLayout(new BoxLayout(panelBottom, BoxLayout.LINE_AXIS));
-		
-		panelBottom.add(pp);
-		
-		panelBottom.add(ap);
-		
-		panelCenter.add(panelBottom);
-	
-	
-		
-		panel.addMouseListener(new CreatureSelection(model, ap));
-		
-		
-	
-	}
-	
-	
-
-	public static void main(String[] args) throws InterruptedException {
-		
-		
-		// THIS IS PSUEDO CODE FOR NOW
-		
-		Main window = new Main();
-		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		window.setSize(610, 720);
-		
-		window.setVisible(true);
-		
-		
-		for (int i = 0; i < 1000000; i++) {
-			
-			
-			window.model.advance();
-			
-			window.panel.repaint();
-			
-			window.pp.repaint();
-			
+		for (int i = 0; i < 1000000; i++){
+			model.advance();
+			view.panel.repaint();
+			view.pp.repaint();
 			Thread.sleep(100);
-			
-		
 		}
-		
-		
-	
 	}
-
-
 }
